4cdde4f1051deb3c446399409d515e1c43f3896b
saudrey <weerawutearth@gmail.com>
2021-03-11 17:05:07 +0700

    c

---
+++ 6220502183_5_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m,i,j;
+
+    scanf("%d",&n);
+    scanf("%d",&m);
+
+    int a[n][n],b[m][m];
+
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; i < n; i++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    
+}
