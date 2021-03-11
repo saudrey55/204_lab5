cf9f51461d4206a58e198e12bab580e60507f0aa
saudrey <weerawutearth@gmail.com>
2021-03-11 15:39:54 +0700

    star

--- 6220502183_2.C
+++ 6220502183_2.C
@@ -3,16 +3,43 @@
 int main()
 {
     int n,i,j,k;
+    char  z[n][((n-1)/2)*2+1];
+    scanf ("%d",&n);
 
-    scanf ("%d",n);
-
-    for ( i = 0; i < n; i++)
-    {
-        for ( j = 0; j < n-2-i ; j++)
+        for ( i = 0; i < n; i++)
         {
-        printf("-");  
+            for ( j = 0; j < ((n-1)/2)*2+1 ; j++)
+            {
+                z[i][j]='-';
+            }
         }
-        printf("\n");
+    for ( i = 0; i < n/2; i++)
+    {
+        z[i][(n-1)/2+i]='*';
+        z[i][(n-1)/2-i]='*';
+        z[n-1-i][(n-1)/2+i]='*';
+        z[n-1-i][(n-1)/2-i]='*';
+    }
+       
+     if (n%2==0)
+    {
+        for ( i = 0; i < n; i++)
+        {
+            for ( j = 0; j < n-1 ; j++)
+            {
+                printf("%c",z[i][j]);
+            }
+        }
+    }
+    else
+    {
+        for ( i = 0; i < n; i++)
+        {
+            for ( j = 0; j < ((n-1)/2)*2+1 ; j++)
+            {
+                printf("%c",z[i][j]);
+            }
+        }
     }
     
 }
